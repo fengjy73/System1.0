48458efe6b166cfa9fc8512baa0d5b458327c3f3
OceanCT <88697499+OceanCT@users.noreply.github.com>
2022-02-28 11:06:18 +0800

    define types

---
+++ Types/types.go
@@ -0,0 +1,95 @@
+package Types
+type UserAuthority int
+const(
+	// For example,if a user is an Owner,his UserAuthority&1 = 1
+	Owner UserAuthority = 1
+	Administator UserAuthority = 2
+	Registered UserAuthority = 4
+	Tourist UserAuthority = 8
+	// A user can be an Administator and Registered in the meantime
+)
+
+type ErrNo int
+const(
+	OK						ErrNo = 0
+	ParamInvaild 			ErrNo = 1
+	DatabaseConnectionErrNo ErrNo = 2
+	PasswordWrong 			ErrNo = 3
+	PermissionDenied 		ErrNo = 4
+
+	UnknownErrNo 			ErrNo = -1
+)
+
+type User struct{
+	UserID string
+	UserName string
+	NickName string 
+	Password string	
+}
+type LoginRequest struct{
+	UserName string `json:"Username" binding:"required"`
+	Password string `json:"Password" binding:"required"`
+}
+// after get logined in successfully, return Cookie Session 
+type LoginResponse struct{
+	Code ErrNo
+	Data struct{
+		UserID string
+	}
+}
+type LogoutRequest struct{
+}
+type LogoutResponse struct{
+	Code ErrNo
+}
+// use for test and retrieve Password
+type WhoAmIRequest struct{
+
+}
+type WhoAmIResponse struct{
+	Code ErrNo
+	Data User
+}
+
+type CreateUserRequest struct{
+	NickName string
+	Username string
+	Password string
+	UserAuthority UserAuthority
+}
+type CreateUserResponse struct{
+	Code ErrNo
+	Data struct {
+		UserID string
+	}
+}
+type GetUserRequest struct{
+	UserID string
+}
+type GetUserResponse struct{
+	Code ErrNo
+	Data User
+}
+type GetUserListRequest struct{
+	Offest int
+	Limit int
+}
+type GetUserListResponse struct{
+	Code ErrNo
+	Data struct{
+		UserList []User
+	}
+}
+type UpdateUserRequest struct{
+	UserID string
+	NickName string
+}
+type UpdateUserResponse struct{
+	Code ErrNo
+}
+type DeleteUserRequest struct{
+	UserID string
+}
+type DeleteUserResponse struct{
+	Code ErrNo
+}
